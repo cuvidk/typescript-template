1f22eebd21bcf9a0c0885210894976c7cc8c9d22
Corvin Tiperciuc <cuvidk@gmail.com>
2022-02-06 17:24:15 +0200

    added ts, eslint, prettier, dotenv

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import 'dotenv/config';
+
+enum Greeting {
+  Usual = 'Hello',
+  Casual = 'Hi',
+  NoFsGiven = 'Norok',
+}
+
+interface Person {
+  name: string;
+  age: number;
+}
+
+const greet = (greet: Greeting, person: Person): string => {
+  return `${greet} ${person.name}. You look awfull for a ${person.age} years old.`;
+};
+
+const main = () => {
+  console.log(
+    greet(Greeting.NoFsGiven, { name: process.env.NAME ?? 'teluap', age: 99 }),
+  );
+};
+
+main();
